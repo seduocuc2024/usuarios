aa91cdf10c5ec30e6a0fd78c93f47716b0f092ff
seduocuc2024 <se.penap@duocuc.cl>
2024-04-15 20:37:26 -0400

    Se agregan validaciones para el controller

--- src/main/java/com/usuarios/aplicacion/controller/UsuarioController.java
+++ src/main/java/com/usuarios/aplicacion/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.usuarios.aplicacion.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.usuarios.aplicacion.model.Usuario;
@@ -15,26 +17,44 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
+
+    private static final Logger log = LoggerFactory.getLogger(UsuarioController.class);
 
     @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping
     public List<Usuario> getAllUsuarios(){
+        log.info("GET /usuarios");
+        log.info("Retornando todos los usuarios");
         return usuarioService.getAllUsuarios();
     }
     
     @GetMapping("/{id}")
-    public Optional<Usuario> getUsuarioById(@PathVariable Long id){
-        return usuarioService.getUsuarioById(id);
+    public ResponseEntity<Object> getUsuarioById(@PathVariable Long id){
+        Optional <Usuario> usuario = usuarioService.getUsuarioById(id);
+
+        if(usuario.isEmpty()){
+            log.error("No se encontro el usuario con ID {}", id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("No se encontro el usuario con ID "+ id));
+        }
+        return ResponseEntity.ok(usuario);
     }
     
     @PostMapping
-    public Usuario createUsuario(@RequestBody Usuario usuario){
-        return usuarioService.createUsuario(usuario);
+    public ResponseEntity<Object> createUsuario(@RequestBody Usuario usuario){
+        Usuario createdUsuario = usuarioService.createUsuario(usuario);
+        if(createdUsuario == null){
+            log.error("Error al crear al usuario {}", usuario);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Error al crear al usuario"));
+        }
+        return ResponseEntity.ok(createdUsuario);
     }
     
     @PutMapping("/{id}")
@@ -47,4 +67,16 @@ public class UsuarioController {
         usuarioService.deleteUsuario(id);           
     }
     
+    static class ErrorResponse{
+        private final String message;
+
+        public ErrorResponse(String message){
+            this.message = message;
+        }
+
+        public String getMessage(){
+            return message;
+        }
+    }
+
 }
