8ad67f42ec22371d32285249826e714c876d7cc3
seduocuc2024 <se.penap@duocuc.cl>
2024-05-03 17:29:27 -0400

    Se agrega wallet

--- src/main/java/com/usuarios/aplicacion/controller/UsuarioController.java
+++ src/main/java/com/usuarios/aplicacion/controller/UsuarioController.java
@@ -4,17 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.usuarios.aplicacion.model.Usuario;
 import com.usuarios.aplicacion.service.UsuarioService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
